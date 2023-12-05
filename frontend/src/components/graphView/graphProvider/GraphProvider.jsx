6e121b4772e8f5515508877dcdb43e90461d3297
Basti <bastian.settele@rwth-aachen.de>
2023-12-05 14:24:36 +0100

    frontend changes

--- frontend/src/components/graphView/graphProvider/GraphProvider.jsx
+++ frontend/src/components/graphView/graphProvider/GraphProvider.jsx
@@ -1,39 +1,24 @@
-import React, { useState, useEffect } from 'react'
-import GraphRenderer from '../graphRenderer'
+import React from 'react'
+
+import GraphRenderer from './graphRenderer'
 
 const GraphProvider = () => {
-  const [graphData, setGraphData] = useState(null)
-
-  // Default graph structure
-  const defaultGraph = {
+  const graphData = {
     nodes: [
-      { id: 'node1' },
-      { id: 'node2' },
-      { id: 'node3' }
+      { id: 'A' },
+      { id: 'B' },
+      { id: 'C' }
     ],
     links: [
-      { source: 'node1', target: 'node2' },
-      { source: 'node2', target: 'node3' }
+      { source: 'A', target: 'B' },
+      { source: 'B', target: 'C' },
+      { source: 'C', target: 'A' }
     ]
   }
 
-  // Fetch graph data from backend
-  const fetchGraphData = () => {
-    // TODO: Implement backend API call
-    // For now, we use the default graph
-    setGraphData(defaultGraph)
-  }
-
-  useEffect(() => {
-    fetchGraphData()
-  }, [])
-
   return (
     <div>
-      {graphData
-        ? <GraphRenderer data={graphData} />
-        : <p>Loading graph...</p>
-      }
+      <GraphRenderer data={graphData}/>
     </div>
   )
 }
