1633d1424287225b4162c52e3161fcd35f34f270
pouya_sh <pouya.esfahani@rwth-aachen.de>
2023-12-05 17:38:18 +0100

    some changes

--- backend/oocc/views.py
+++ backend/oocc/views.py
@@ -1,16 +1,48 @@
 # views.py
+import os
+import json
+from lxml import etree
+import jsonschema
+from django.conf import settings
 
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from .models import EventLog
 from .serializers import EventLogSerializer
-#from .serializers import OCELSerializer
+
 
 class EventLogViewSet(viewsets.ModelViewSet):
     queryset = EventLog.objects.all()
     serializer_class = EventLogSerializer
 
+    def validate_xml(self, xml_data, value):
+            # Construct the path to the XSD schema
+            xsd_folder = 'ocelSchema'
+            xsd_file_path = os.path.join(settings.BASE_DIR, 'oocc', xsd_folder, 'ocel20-schema-xml.xsd')
+
+            # Parse XSD schema
+            schema = etree.XMLSchema(file=xsd_file_path)
+
+            # Parse XML data
+            try:
+                xml_data = etree.fromstring(value.read())
+            except etree.XMLSyntaxError:
+                raise serializers.ValidationError('Invalid XML format.')
+
+            # Validate XML against XSD
+            is_valid = schema.validate(xml_data)
+
+            if not is_valid:
+                raise serializers.ValidationError({'file': 'XML is not valid against XSD.'})
+
+            return value
+            
+    def validate_json(self, json_data, json_schema):
+        {
+            
+        }
+            
     @action(detail=True, methods=['post'])
     def process_file(self, request, pk=None):
         print(request.data)  # Add this line to print request data
--- frontend/src/components/graphView/GraphView.jsx
+++ frontend/src/components/graphView/GraphView.jsx
@@ -1,19 +1,14 @@
-// GraphView.jsx
 import React from "react";
+import * as d3 from "d3";
 import './GraphView.scss';
 
-function GraphView({ isOpen ,processedData}) {
-    return (
-      <section className={`graphView ${isOpen ? 'open' : ''}`}>
-        {/* Display the processed data */}
-        <div>
-        <h3>Processed Data:</h3>
-        <div>{processedData}</div>
-        </div>
-        {/* Your GraphView content */}
-      </section>
-    );
-  }
-  
+function GraphView({ isOpen }) {
+
+  return (
+    <section className={`graphView ${isOpen ? 'open' : ''}`}>
+      {/* Display the D3.js graph */}
+    </section>
+  );
+}
 
 export default GraphView;
