cdd2bd3e6bc629401d9d99ddf722ecb6107e6889
pouya_sh <pouya.esfahani@rwth-aachen.de>
2023-12-15 12:50:49 +0100

    added file_proccesing logic and Evenlog validation

--- backend/oocc/views.py
+++ backend/oocc/views.py
@@ -1,15 +1,16 @@
 # views.py
-
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from .models import EventLog
 from .serializers import EventLogSerializer
 from django.http import JsonResponse
-##ocpa## 
-from ocpa.algo.discovery.ocpn import algorithm as ocpn_discovery_factory
-from ocpa.objects.log.importer.ocel import factory as ocel_import_factory
-import traceback
+import sys
+import os
+# Add the path to the 'scripts' directory
+sys.path.append(os.path.join(os.path.dirname(__file__), 'scripts'))
+
+from oocc.scripts.ProcessDiscovery import validate_file
 #####
 import time
 
@@ -26,38 +27,28 @@ class EventLogViewSet(viewsets.ModelViewSet):
             # Check if the file has already been processed
             if event_log.processed_data:
                 return JsonResponse({'message': 'File already processed'}, status=200)
+             # Validate the uploaded file (modify this based on your validation logic)
+            is_valid_file = validate_file(event_log.file.path)
 
-            # Process the event log using OCPA
-            try:
-                print(f"file path is: ********* :{str(event_log.file.path)}")
-                ocel = ocel_import_factory.apply(file_path=event_log.file.path)
-                
-            except Exception as e:
-                # Log detailed error information
-                print(f"Error importing OCEl file: {str(e)}")
-                traceback.print_exc()  # Print the traceback for detailed error information
-                return JsonResponse({'error': 'Invalid OCEl file format'}, status=400)
+            if not is_valid_file:
+                return JsonResponse({'error': 'Invalid file format or content'}, status=400)
 
             try:
-                ocpn = ocpn_discovery_factory.apply(ocel)
+                # Your processing logic here
+
+                return JsonResponse({'message': 'File processed successfully'}, status=200)
             except Exception as e:
                 # Log detailed error information
-                print(f"Error applying OCPA algorithm: {str(e)}")
-                traceback.print_exc()  # Print the traceback for detailed error information
-                return JsonResponse({'error': 'Error applying OCPA algorithm'}, status=500)
+                print(f"Error processing file: {str(e)}")
+                return JsonResponse({'error': 'Internal Server Error'}, status=500)
 
-            # Store the OCPA results in the database
-            event_log.processed_data = {'petri_net': ocpn.to_dict()}
-            event_log.save()
-
-            return JsonResponse({'message': 'File processed successfully'}, status=200)
-
-        except EventLog.DoesNotExist:
-            return JsonResponse({'error': 'No EventLog found'}, status=404)
+            # Process the event log using PM4Py
+            #try:   
+            #except Exception as e:
+            # Log detailed error information
 
         except Exception as e:
             # Log detailed error information
             print(f"Error processing file: {str(e)}")
-            traceback.print_exc()  # Print the traceback for detailed error information
             return JsonResponse({'error': 'Internal Server Error'}, status=500)
         
--- frontend/src/components/headerComponent/uploadModule/UploadModule.jsx
+++ frontend/src/components/headerComponent/uploadModule/UploadModule.jsx
@@ -5,7 +5,7 @@ import Modal from '@mui/material/Modal'
 import Box from '@mui/material/Box'
 import UploadButton from './uploadButton/UploadButton'
 
-function UploadModule ({ closeModal, onUploadSuccess, onProcessSuccess, updateProgress}) {
+function UploadModule ({ closeModal, onUploadSuccess, updateProgress}) {
   const [file, setFile] = useState(null);  // State to keep track of the selected file
    
   // Function to handle file selection
@@ -15,36 +15,59 @@ function UploadModule ({ closeModal, onUploadSuccess, onProcessSuccess, updatePr
 
   // Function to handle file upload
   const handleFileUpload = async () => {
-    closeModal()
-
+    closeModal();
+  
     if (!file) {
       console.log('No file selected');
       return;
     }
-
+  
     const formData = new FormData();
     formData.append('file', file);
-
+  
     try {
       const xhr = new XMLHttpRequest();
-
+  
       xhr.upload.addEventListener('progress', (event) => {
         if (event.lengthComputable) {
           const progress = (event.loaded / event.total) * 100;
           updateProgress(progress);
         }
       });
-
+  
       xhr.open('POST', 'http://localhost:8000/api/eventlogs/');
       xhr.setRequestHeader('Accept', 'application/json');
-
-      xhr.send(formData);
-
+  
       xhr.onload = async () => {
         if (xhr.status === 201) {
           try {
             const result = JSON.parse(xhr.responseText);
             console.log('Upload successful. Result:', result);
+  
+            // Call the onUploadSuccess function if it's defined
+            if (onUploadSuccess) {
+              onUploadSuccess(result);
+            }
+  
+            // Trigger the process_file function immediately after successful upload
+            try {
+              const processResponse = await fetch(`http://localhost:8000/api/eventlogs/${result.id}/process_file/`, {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+                
+              });
+  
+              if (processResponse.status === 200) {
+                const processResult = await processResponse.json();
+                console.log('File processing successful:', processResult);
+              } else {
+                console.error('File processing failed:', processResponse.statusText);
+              }
+            } catch (processError) {
+              console.error('Error during file processing:', processError);
+            }
           } catch (error) {
             console.error('Error parsing JSON:', error);
           }
@@ -52,12 +75,15 @@ function UploadModule ({ closeModal, onUploadSuccess, onProcessSuccess, updatePr
           console.error('Upload failed:', xhr.responseText);
         }
       };
+  
+      // Call xhr.send after setting up the onload handler
+      xhr.send(formData);
     } catch (error) {
       console.error('Error during upload:', error);
     } finally {
       setFile(null); // Reset the file selection
     }
-  };
+  };  
 
 
   return (
